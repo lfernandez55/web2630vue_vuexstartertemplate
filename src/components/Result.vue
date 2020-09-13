32e87c58bf075215c73c26f24879451b36f0cf3a
lfernandez55 <lfernandez@weber.edu>
2020-09-12 22:10:45 -0600

    7b Mapping Getters Finished

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -3,21 +3,28 @@
   <h2>Result</h2>
   <p>Counter is (not using getter): {{ this.$store.state.counter }}</p> 
   <p>Double Counter (using getter -- works like computed function) is {{counter}}</p>
-  <p>String Thing: {{stringMaThing}}</p>
+  <p>String Thing: {{stringThing}}</p>
   <hr>
   </div>
 
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
+
 export default {
   computed:{
-    counter(){
-      return this.$store.getters.doubleCounter
-    },
-    stringMaThing(){
-      return this.$store.getters.stringThing
-    }
+    ...mapGetters([
+      'doubleCounter',
+      'stringThing'
+    ])
+
+    // counter(){
+    //   return this.$store.getters.doubleCounter
+    // },
+    // stringMaThing(){
+    //   return this.$store.getters.stringThing
+    // }
   }
 }
 </script>
